fc601a79b09b17310910fea9c3585bbf1d7d6593
knttnk <61683744+knttnk@users.noreply.github.com>
2023-05-21 21:49:53 +0900

    my-card

--- animation.js
+++ animation.js
@@ -4,16 +4,9 @@
 
     window.addEventListener('scroll', () => {
         const scroll = window.scrollY;
-        fixedHeader.style.opacity = Math.min(
-            100,
-            Math.max(0, scroll - 50) / 2.0,
-        ) + "%";
-        // if (window.scrollY > 80) {
-        //     console.log("animation.js log point 2")
-        //     fixedHeader.classList.add('is-show')
-        // } else {
-        //     console.log("animation.js log point 3")
-        //     fixedHeader.classList.remove('is-show');
-        // }
+        const criteriaNotVisible = window.screen.availHeight * 0.5;
+        const criteriaVisible = window.screen.availHeight * 0.6;
+
+        fixedHeader.style.opacity = (scroll - criteriaNotVisible) / (criteriaVisible - criteriaNotVisible);
     })
 }());
