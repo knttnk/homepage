c36a639bce9a67ba0841c597d7ea665e11356230
knttnk <61683744+knttnk@users.noreply.github.com>
2023-05-21 22:24:39 +0900

    スマホに対応

--- animation.js
+++ animation.js
@@ -4,8 +4,9 @@
 
     window.addEventListener('scroll', () => {
         const scroll = window.scrollY;
+        const rem = getComputedStyle(document.documentElement).fontSize;
         const criteriaNotVisible = window.screen.availHeight * 0.5;
-        const criteriaVisible = window.screen.availHeight * 0.6;
+        const criteriaVisible = window.screen.availHeight * 0.7;
 
         fixedHeader.style.opacity = (scroll - criteriaNotVisible) / (criteriaVisible - criteriaNotVisible);
     })
