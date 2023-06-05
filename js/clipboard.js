2e32c73fe40d2f6f7db2d5fdf3d94fcbc448db72
knttnk <61683744+knttnk@users.noreply.github.com>
2023-06-05 20:13:37 +0900

    連絡先

---
+++ js/clipboard.js
@@ -0,0 +1,21 @@
+function copyMail() {
+    // スクレイピングに引っかからないよう難読化のつもり
+    const d = "eom" + "." + "mech" + "." + "eng" + "." + "osak" + "a-" + "u." + "ac" + "." + "j" + "p";
+    const name = "kenta";
+    navigator.clipboard.writeText(
+        name + "." + "tanaka" + "@" + d
+    );
+}
+
+function copyAddress() {
+    navigator.clipboard.writeText(
+        "〒565-0871 大阪府吹田市山田丘２−１"
+    );
+}
+
+
+function copyPhone() {
+    navigator.clipboard.writeText(
+        "06-6879-7267"
+    );
+}
