19e5e5ddc57a161337ebdb1104d69456c6d5ee7b
knttnk <61683744+knttnk@users.noreply.github.com>
2024-09-04 13:58:26 +0900

    住所や電話番号がコピーされないバグを修正

--- js/clipboard.js
+++ js/clipboard.js
@@ -7,15 +7,28 @@ function copyMail() {
     );
 }
 
-function copyAddress() {
-    let address1 = i18next.t('contact.address1');
-    let address2 = i18next.t('contact.address2');
-    navigator.clipboard.writeText(
-        `${address1} ${address2}`
-    );
+function copyAddress(lang) {
+    if (lang === "ja") {
+        let address1 = "〒565-0871";
+        let address2 = "大阪府吹田市山田丘２−１ M4棟 505号室";
+        navigator.clipboard.writeText(
+            `${address1} ${address2}`
+        );
+    } else {
+        let address1 = "#505 M4 2-1 Yamadaoka,";
+        let address2 = "Suita, Osaka 565-0871, Japan";
+        navigator.clipboard.writeText(
+            `${address1} ${address2}`
+        );
+    }
 }
 
-function copyPhone() {
-    let phone = i18next.t('contact.phone1');
-    navigator.clipboard.writeText(phone);
+function copyPhone(lang) {
+    if (lang === "ja") {
+        let phone = "06-6879-7267";
+        navigator.clipboard.writeText(phone);
+    } else {
+        let phone = "+81-6-6879-7267";
+        navigator.clipboard.writeText(phone);
+    }
 }
