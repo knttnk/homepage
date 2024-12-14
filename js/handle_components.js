da833268ab71ce306e60fa190652baaa6639123f
knttnk <61683744+knttnk@users.noreply.github.com>
2024-12-15 01:37:42 +0900

    ヘッダーの高さが読み込み前とあとで変わらないように

--- js/handle_components.js
+++ js/handle_components.js
@@ -1,10 +1,16 @@
 (
   async function () {
-    const header = await fetch('components/header.html').then(response => response.text());
-    const footer = await fetch('components/footer.html').then(response => response.text());
-
     const mainContent = document.getElementById('main-content');
+
+    // ヘッダーを置換
+    const header = await fetch('components/header.html').then(response => response.text());
+    const preHeader = document.getElementsByClassName('pre-header')[0];
+    preHeader.remove();
     mainContent.insertAdjacentHTML('beforeBegin', header);
+
+
+    // フッターを挿入
+    const footer = await fetch('components/footer.html').then(response => response.text());
     mainContent.insertAdjacentHTML('afterEnd', footer);
   }()
 );
